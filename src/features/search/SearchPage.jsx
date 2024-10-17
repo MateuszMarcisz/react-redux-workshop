49e3e9de768d71587e50a0348a120c562ae28e4f
MateuszMarcisz <mateusz.marciszm@gmail.com>
2024-10-17 16:58:37 +0200

    implemented search movie functionality and added some basic styling for movie list

--- src/features/search/SearchPage.jsx
+++ src/features/search/SearchPage.jsx
@@ -1,10 +1,42 @@
 import {Button} from "react-bootstrap";
+import {useDispatch, useSelector} from "react-redux";
+import {fetchMovies} from "../movie/movieSlice.js";
+import {useState} from "react";
+import MovieList from "../../components/MovieList.jsx";
 
 const SearchPage = () => {
+    const dispatch = useDispatch();
+    const {movieList, isLoading, error} = useSelector((state) => state.movies);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+        dispatch(fetchMovies(searchTerm));
+        setSearchTerm('');
+    };
+
     return (
-        <div>
-            <input/>
-            <Button variant="primary">Search Movies</Button>
+        <div className='movie-container'>
+            <div>
+                <form onSubmit={handleSearch}>
+                    <input
+                        type='text'
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                        placeholder="Search movie by the name"
+                    />
+                    <Button
+                        type='submit'
+                        variant="primary">
+                        Search Movies
+                    </Button>
+                </form>
+            </div>
+            <div>
+                {isLoading && <p>Loading...</p>}
+                {error && <p>Error: {error}</p>}
+                {movieList.length > 0 && <MovieList movies={movieList} showPoster={true} showYear={true} />}
+            </div>
         </div>
     );
 };
