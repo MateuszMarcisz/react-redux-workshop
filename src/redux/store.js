c9adc577637741b5b062e43ef632e4626feb551b
MateuszMarcisz <mateusz.marciszm@gmail.com>
2024-10-18 13:22:49 +0200

    toWatch Checkxbox implemented, basic toWatchMovieList implemented, some bugfix needed

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,14 @@
 import {configureStore} from "@reduxjs/toolkit";
 import movieReducer from '../features/movie/movieSlice.js';
+import ratingReducer from '../features/rating/ratingSlice.js';
+import toWatchMoviesReducer from '../features/toWatchMovies/toWatchMoviesSlice.js';
 
 
 const store = configureStore({
     reducer: {
         movies: movieReducer,
+        ratings: ratingReducer,
+        toWatchMovies: toWatchMoviesReducer,
     },
 });
 
